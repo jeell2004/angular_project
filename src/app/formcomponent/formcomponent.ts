771a3d36b3247c6765dd0e0a57034111358cd38f
jeell2004 <jeellimbani2004@gmail.com>
2026-01-14 21:58:59 +0530

    small change

--- src/app/formcomponent/formcomponent.ts
+++ src/app/formcomponent/formcomponent.ts
@@ -8,24 +8,29 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './formcomponent.html',
   styleUrl: './formcomponent.css',
 })
-export class Formcomponent {userForm!: FormGroup;
+export class Formcomponent {
+userForm!: FormGroup;
   users: any[] = [];
-  editIndex: number | null = null;
+  editIndex = -1;  
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.userForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', Validators.required]
     });
 
     this.loadUsers();
   }
 
   loadUsers() {
-    const data = localStorage.getItem('users');
-    this.users = data ? JSON.parse(data) : [];
+    var data = localStorage.getItem('users');
+    if (data) {
+      this.users = JSON.parse(data);
+    } else {
+      this.users = [];
+    }
   }
 
   saveUsers() {
@@ -33,19 +38,23 @@ export class Formcomponent {userForm!: FormGroup;
   }
 
   submit() {
-    if (this.userForm.invalid) return;
+    if (this.userForm.invalid) {
+      return;
+    }
 
-    if (this.editIndex !== null) {
-      this.users[this.editIndex] = {
-        ...this.users[this.editIndex],
-        ...this.userForm.value,
-      };
-      this.editIndex = null;
+    var userData = {
+      name: this.userForm.value.name,
+      email: this.userForm.value.email
+    };
+
+    if (this.editIndex >= 0) {
+      // UPDATE
+      this.users[this.editIndex].name = userData.name;
+      this.users[this.editIndex].email = userData.email;
+      this.editIndex = -1;
     } else {
-      this.users.push({
-        id: Date.now(),
-        ...this.userForm.value,
-      });
+      // ADD
+      this.users.push(userData);
     }
 
     this.saveUsers();
@@ -54,12 +63,14 @@ export class Formcomponent {userForm!: FormGroup;
 
   editUser(index: number) {
     this.editIndex = index;
-    this.userForm.patchValue(this.users[index]);
+    this.userForm.setValue({
+      name: this.users[index].name,
+      email: this.users[index].email
+    });
   }
 
   deleteUser(index: number) {
     this.users.splice(index, 1);
     this.saveUsers();
   }
-
 }
